a634dffab51b4d328a6875c8702ed9c467ded09b
AbrahamNevin <nevinabr025@gmail.com>
2026-02-14 10:17:57 +0530

    Initial Commit

---
+++ BIPTYA/BIPTYAApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIPTYAApp.swift
+//  BIPTYA
+//
+//  Created by Nevin Abraham on 14/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BIPTYAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
